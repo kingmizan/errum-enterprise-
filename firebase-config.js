7e8c3da5de6f7962265ebad8285fa34cc3ef2ef9
kingmizan <71051740+kingmizan@users.noreply.github.com>
2025-10-12 15:37:50 +0500

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,21 @@
+// firebase-config.js
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-firestore.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAvjLh6aRRUyrJDUtywBbOUwTLH4hWX9e4",
+    authDomain: "milon-ltd.firebaseapp.com",
+    projectId: "milon-ltd",
+    storageBucket: "milon-ltd.appspot.com",
+    messagingSenderId: "780532796920",
+    appId: "1:780532796920:web:433cb112e3836c79e35a0d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Export the services you need in other files
+export const auth = getAuth(app);
+export const db = getFirestore(app);
